5be83ce88052e483b924126c6da58b4a5fe66312
TCewTB0312 <trongson03121999@gmail.com>
2018-02-01 16:34:48 +0700

    tao chuong trinh main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"nhap hai so can tinh:";
+	cin>>a;
+	cin>>b;
+	cout<<"tong :"<<a+b<<endl;
+	cout<<"hieu :"<<a-b<<endl;
+	cout<<"thuong :"<<a/b<<endl;
+	cout<<"tich :"<<a*b<<endl<<"\a";
+	return 0;
+}
